673b276656bbde36f8cf3ceb0ab41f9c8bb40904
OverlordZorn <dennisAzorn@gmail.com>
2025-10-13 03:52:21 +0200

    Update repos_data.py

--- github-label-updater/repos_data.py
+++ github-label-updater/repos_data.py
@@ -6,12 +6,7 @@ Supports both personal and organization repositories.
 REPOSITORIES = [
     # Personal repositories (your GitHub username)
     {"owner": "overlordZorn", "repo": "misc"},
-    #{"owner": "overlordZorn", "repo": "arma3-vehicles"},
 
     # Organization repositories (organization name as owner)
-    #{"owner": "ArmADevTeam", "repo": "mod_ai_enhancer"},
-    #{"owner": "ArmADevTeam", "repo": "advanced_maps"},
-
-    # Another organization
-    #{"owner": "FriendModder", "repo": "custom_vehicles"},
+    {"owner": "CVO-Org", "repo": "CVO-Auxiliary"},
 ]
